0e9db3c9bf236f8fb370c60b90695376020122c8
Ortem-T <plugins@zeroqode.com>
2024-03-29 16:31:35 +0100

    Obfuscated

---
+++ actionsObfuscated/AAC-850m6/client.js
@@ -0,0 +1,64 @@
+function(properties, context){
+	function _0x3af1() {
+    var _0x50d5e5 = [
+        'querySelector',
+        'selectedIndex',
+        '6854302lnchbO',
+        '6973635KPDvJG',
+        '343948PDZQGf',
+        '\x20>\x20span\x20>\x20input.tt-input',
+        '3783282nHylOu',
+        'value',
+        '5530fwHMjB',
+        '1035mIBRMK',
+        'input_id',
+        '60clVDrY',
+        'input_type',
+        'Input/Multiline\x20Input',
+        '15183LpDUKi',
+        'getElementById',
+        '11019504UTrvpd',
+        'Searchbox',
+        '2962QMDFOv',
+        'Dropdown'
+    ];
+    _0x3af1 = function () {
+        return _0x50d5e5;
+    };
+    return _0x3af1();
+}
+function _0x46a3(_0x322cc8, _0x4b1be0) {
+    var _0x3af128 = _0x3af1();
+    return _0x46a3 = function (_0x46a340, _0x3b3069) {
+        _0x46a340 = _0x46a340 - 0x1f3;
+        var _0x2d6c19 = _0x3af128[_0x46a340];
+        return _0x2d6c19;
+    }, _0x46a3(_0x322cc8, _0x4b1be0);
+}
+(function (_0xb022f6, _0x36c94f) {
+    var _0x4d741d = _0x46a3, _0x19afcc = _0xb022f6();
+    while (!![]) {
+        try {
+            var _0x1fdc2a = parseInt(_0x4d741d(0x1fa)) / 0x1 * (-parseInt(_0x4d741d(0x203)) / 0x2) + parseInt(_0x4d741d(0x1fc)) / 0x3 * (parseInt(_0x4d741d(0x1f5)) / 0x4) + parseInt(_0x4d741d(0x1f4)) / 0x5 + parseInt(_0x4d741d(0x1f7)) / 0x6 + parseInt(_0x4d741d(0x1f3)) / 0x7 + -parseInt(_0x4d741d(0x201)) / 0x8 + -parseInt(_0x4d741d(0x1ff)) / 0x9 * (parseInt(_0x4d741d(0x1f9)) / 0xa);
+            if (_0x1fdc2a === _0x36c94f)
+                break;
+            else
+                _0x19afcc['push'](_0x19afcc['shift']());
+        } catch (_0x3d5ff8) {
+            _0x19afcc['push'](_0x19afcc['shift']());
+        }
+    }
+}(_0x3af1, 0xd7178));
+function client(_0xf91098, _0x26c18a) {
+    var _0x1afc9d = _0x46a3;
+    if (_0xf91098[_0x1afc9d(0x1fd)] === _0x1afc9d(0x202))
+        document[_0x1afc9d(0x205)]('#' + _0xf91098[_0x1afc9d(0x1fb)] + _0x1afc9d(0x1f6))['value'] = '';
+    else {
+        if (_0xf91098[_0x1afc9d(0x1fd)] === _0x1afc9d(0x1fe))
+            document[_0x1afc9d(0x200)](_0xf91098['input_id'])[_0x1afc9d(0x1f8)] = '';
+        else
+            _0xf91098[_0x1afc9d(0x1fd)] === _0x1afc9d(0x204) && (document[_0x1afc9d(0x200)](_0xf91098[_0x1afc9d(0x1fb)])[_0x1afc9d(0x206)] = 0x0);
+    }
+}
+	client(properties, context);
+}
---
+++ actionsObfuscated/AAC-850m6/server.js
@@ -0,0 +1,5 @@
+function(properties, context){
+	function server(_0x567abd, _0x133dd0) {
+}
+	server(properties, context);
+}
